07069eca891843df601d7945ccc4dc335a25deb0
dyakubu <dev.daniel.doy@gmail.com>
2026-09-21 20:22:55 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+
+        groupedAnagrams = defaultdict(list)
+
+        def charMap(s):
+
+            freqs = [0] * 26
+            for char in s:
+                freqs[ord(char)-ord('a')] += 1
+            return freqs
+        
+        for s in strs:
+            charFreq = tuple(charMap(s))
+            groupedAnagrams[charFreq].append(s)
+
+        res = [v for k, v in groupedAnagrams.items()]
+
+        return res
